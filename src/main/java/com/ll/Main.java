4067adf2fed9ef0837ac2112de4076a579041b10
gayoi <dbs9476@naver.com>
2023-10-31 09:31:42 +0900

    step4

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -4,5 +4,10 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hi");
         System.out.println("Hello");
+        System.out.println("java");
+        System.out.println("java");
+
+
+
     }
 }
